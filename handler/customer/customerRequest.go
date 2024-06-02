a6b22eef7a6aa5112d6cbfc41f8fa5d206bd205b
Bruno Roboredo <vital.bcroboredo@terceiros.mag.com.br>
2024-06-02 17:56:32 +0100

    feat(customer): added customer handlers

--- handler/customer/customerRequest.go
+++ handler/customer/customerRequest.go
@@ -1,3 +1,48 @@
 package customer
 
-// TODO:
+import (
+	"errors"
+	"github.com/broboredo/locapp-api/handler"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type CreateCustomerRequest struct {
+	Name    string `json:"name" validate:"required,min=3,max=255"`
+	Address string `json:"description"`
+	Phone   string `json:"phone"`
+	Notes   string `json:"notes"`
+}
+
+type UpdateCustomerRequest struct {
+	Name    string `json:"name" validate:"min=3,max=255"`
+	Address string `json:"description"`
+	Phone   string `json:"phone"`
+	Notes   string `json:"notes"`
+}
+
+type IdIsRequired struct{}
+
+func (req *CreateCustomerRequest) Validate(context *gin.Context) error {
+	return handler.ValidateProductRequest(req, context)
+}
+
+func (req *UpdateCustomerRequest) Validate(context *gin.Context) error {
+	r := IdIsRequired{}
+	err := r.Validate(context)
+	if err != nil {
+		return err
+	}
+
+	return handler.ValidateProductRequest(req, context)
+}
+
+func (req *IdIsRequired) Validate(context *gin.Context) error {
+	id := context.Param("id")
+	if id == "" {
+		handler.SendError(context, http.StatusBadRequest, "no customer id provided")
+		return errors.New("no customer id provided")
+	}
+
+	return nil
+}
--- handler/customer/cutomerHandler.go
+++ handler/customer/cutomerHandler.go
@@ -1,25 +1,121 @@
 package customer
 
 import (
+	"fmt"
+	"github.com/broboredo/locapp-api/handler"
+	"github.com/broboredo/locapp-api/schemas"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Create(context *gin.Context) {
-	// TODO:
+	var req CreateCustomerRequest
+	if err := req.Validate(context); err != nil {
+		return
+	}
+
+	customer := schemas.Customer{
+		Name:    req.Name,
+		Phone:   req.Phone,
+		Notes:   req.Notes,
+		Address: req.Address,
+	}
+
+	if err := handler.Db.Create(&customer).Error; err != nil {
+		handler.Logger.Errorf("error when try create new customer %v", err)
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	handler.SendSuccess(context, customer, http.StatusCreated)
 }
 
 func Update(context *gin.Context) {
-	// TODO:
+	var req UpdateCustomerRequest
+	if err := req.Validate(context); err != nil {
+		return
+	}
+
+	customer := schemas.Customer{}
+
+	id := context.Param("id")
+	if err := handler.Db.First(&customer, id).Error; err != nil {
+		handler.SendError(context, http.StatusNotFound, fmt.Sprintf("Customer with id: %s not found", id))
+		return
+	}
+
+	if req.Name != "" {
+		customer.Name = req.Name
+	}
+
+	if req.Notes != "" {
+		customer.Notes = req.Notes
+	}
+
+	if req.Address != "" {
+		customer.Address = req.Address
+	}
+
+	if req.Phone != "" {
+		customer.Phone = req.Phone
+	}
+
+	if err := handler.Db.Save(&customer).Error; err != nil {
+		handler.Logger.Errorf("error updating opening: %v", err.Error())
+		handler.SendError(context, http.StatusInternalServerError, "error updating opening")
+		return
+	}
+
+	handler.SendSuccess(context, customer)
 }
 
 func List(context *gin.Context) {
-	// TODO:
+	var customers []schemas.Customer
+
+	if err := handler.Db.Find(&customers).Error; err != nil {
+		handler.SendError(context, http.StatusInternalServerError, "error listing customers")
+		return
+	}
+
+	handler.SendSuccess(context, customers)
 }
 
 func Find(context *gin.Context) {
-	// TODO:
+	var req IdIsRequired
+	if err := req.Validate(context); err != nil {
+		return
+	}
+
+	customer := schemas.Customer{}
+
+	id := context.Param("id")
+	if err := handler.Db.First(&customer, id).Error; err != nil {
+		handler.SendError(context, http.StatusNotFound, fmt.Sprintf("Customer with id: %s not found", id))
+		return
+	}
+
+	handler.SendSuccess(context, customer)
 }
 
 func Delete(context *gin.Context) {
-	// TODO:
+	var req IdIsRequired
+	if err := req.Validate(context); err != nil {
+		return
+	}
+
+	customer := schemas.Customer{}
+
+	db := handler.Db
+	id := context.Param("id")
+	if err := db.First(&customer, id).Error; err != nil {
+		handler.SendError(context, http.StatusNotFound, fmt.Sprintf("Customer with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&customer).Error; err != nil {
+		handler.SendError(context, http.StatusInternalServerError, fmt.Sprintf("error deleting Customer with id: %s", id))
+		return
+	}
+
+	handler.SendSuccess(context, customer)
 }
